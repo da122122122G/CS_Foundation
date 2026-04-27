60692bed44c28c38b0126a0a4a448c5d488f0ff8
da122122122G <daichi.712.houzuki@gmail.com>
2026-04-27 16:14:21 +0900

    Ex_02_02_force

--- Ex_02_02/Program.cs
+++ Ex_02_02/Program.cs
@@ -4,8 +4,6 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("文字列を入力してください->");
-        string? value = Console.ReadLine();//変数valueに入力された値を代入
-        Console.WriteLine( value ); //入力された値を表示
+        Console.WriteLine("Hello, World!");
     }
 }

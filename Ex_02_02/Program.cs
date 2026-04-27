13e8f451142b85196294f80992fb644452797e84
da122122122G <daichi.712.houzuki@gmail.com>
2026-04-27 16:24:39 +0900

    2.4

--- Ex_02_02/Program.cs
+++ Ex_02_02/Program.cs
@@ -4,6 +4,8 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("文字列を入力してください->");
+        string value = Console.ReadLine();//変数valueに入力された値を代入
+        Console.WriteLine(value); //入力された値を表示
     }
 }
--- Example/Program.cs
+++ Example/Program.cs
@@ -5,6 +5,18 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+// 数値の演算
+int a = 10;
+Console.WriteLine($"a += 3 -> {a += 3}");
+Console.WriteLine($"a -= 3 -> {a -= 3}");
+Console.WriteLine($"a *= 3 -> {a *= 3}");
+Console.WriteLine($"a /= 3 -> {a /= 3}");
+Console.WriteLine($"a %= 3 -> {a %= 3}");
+// 文字列の連結
+string b = "ABC";
+Console.WriteLine($"b += XYZ -> {b += "XYZ"}");
+// 文字列と他のデータ型の結合
+string c = "LMN";
+Console.WriteLine($"c += 1   -> {c += 1}");
     }
 }

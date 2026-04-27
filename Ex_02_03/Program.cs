be357166040d2b7d09e7dd8b91ca961cfb3e27fc
da122122122G <daichi.712.houzuki@gmail.com>
2026-04-27 17:04:26 +0900

    Ex_02_03

---
+++ Ex_02_03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Formats.Asn1;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Ex_02_03;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("数値を入力してください->");
+        var value = System.Console.ReadLine();
+          
+        System.Console.WriteLine($"5を加算した結果は{int.Parse(value) + 5}です");
+        System.Console.WriteLine($"5を減算した結果は{int.Parse(value) - 5}です");
+
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -5,18 +5,18 @@ static class Program
 {
     static void Main(string[] args)
     {
-        var a = 10;
-        Console.WriteLine($"a < 10  -> {a < 10}");
-        Console.WriteLine($"a < 20  -> {a < 20}");
-        Console.WriteLine($"a <  0  -> {a < 0}");
-        Console.WriteLine($"a > 10  -> {a > 10}");
-        Console.WriteLine($"a > 20  -> {a > 20}");
-        Console.WriteLine($"a >  0  -> {a > 0}");
-        Console.WriteLine($"a <= 10 -> {a <= 10}");
-        Console.WriteLine($"a <= 20 -> {a <= 20}");
-        Console.WriteLine($"a <=  0 -> {a <= 0}");
-        Console.WriteLine($"a >= 10 -> {a >= 10}");
-        Console.WriteLine($"a >= 20 -> {a >= 20}");
-        Console.WriteLine($"a >=  0 -> {a >= 0}");
+        // 2教科の平均点が合格ライン（60点）を超えているか？
+        int mathScore = 80;
+        int englishScore = 50;
+        int passingGrade = 60;
+
+        // 【優先順位のポイント】
+        // 1. カッコ ( ) の中が最優先
+        // 2. 次に割り算 / が行われる
+        // 3. 最後に比較 > が行われる
+        bool isPassed = (mathScore + englishScore) / 2 > passingGrade;
+
+        Console.WriteLine($"数学{mathScore}点、英語{englishScore}点。");
+        Console.WriteLine($"平均点は{passingGrade}点を超えているか：{isPassed}");
     }
 }

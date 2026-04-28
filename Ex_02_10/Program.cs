cbab7e74bb92e109e58af529096fddbbd33baea1
da122122122G <daichi.712.houzuki@gmail.com>
2026-04-28 12:09:01 +0900

    Ex_02_10

---
+++ Ex_02_10/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Ex_02_10;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        string[] stations = { "品川", "大崎", "五反田", "目黒", "恵比寿", "渋谷", "原宿", "代々木", "新宿" };
+        foreach (string station in stations)
+        {
+            Console.WriteLine(station);
+        }
+    }
+}

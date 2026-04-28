0d739448a40cfad2a0ebd50feb7d5aa288658cce
da122122122G <daichi.712.houzuki@gmail.com>
2026-04-28 11:12:38 +0900

    Ex_02_07

---
+++ Ex_02_07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace Ex_02_07;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        int Number = int.Parse(Console.ReadLine());
+
+        if(Number == 0)
+        {
+            Console.WriteLine("0です。");
+        }
+
+        else if(Number > 0)
+        {
+            Console.WriteLine("正の値です。");
+        }
+
+        else
+        {
+            Console.WriteLine("負の値です。");
+        }
+
+        
+    }
+}

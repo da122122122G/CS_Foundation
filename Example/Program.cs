491e99cdf77b5f52db4b417cb7d4e46e44587494
da122122122G <daichi.712.houzuki@gmail.com>
2026-04-28 13:36:41 +0900

    FizzBuzz

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Formats.Asn1;
+using System.Globalization;
 
 namespace Example;
 
@@ -7,14 +8,33 @@ static class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i <= 6; i++)
+        for (int i = 1; i <= 100; i++)
         {
-            if (i == 2)
+            if (i == 100)
             {
-                continue;
+                Console.Write("Buzz");
             }
-            Console.WriteLine($"i = {i}");
+
+            else if (i % 15 == 0)
+            {
+                Console.Write("FizzBuzz,");
+            }
+
+            else if (i % 3 == 0)
+            {
+                Console.Write("Fizz,");
+            }
+
+            else if (i % 5 == 0)
+            {
+                Console.Write("Buzz,");
+            }
+
+            else
+            {
+                Console.Write(i + ",");
+            }
+
         }
-        Console.WriteLine("終了");
     }
 }

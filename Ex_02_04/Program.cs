6b244af2e3ed178c39aa1ef97a1a0153e4b5b36d
da122122122G <daichi.712.houzuki@gmail.com>
2026-04-28 10:12:09 +0900

    Ex_02_04

---
+++ Ex_02_04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex_02_04;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        int[] insert = new int[3];
+
+        Console.Write("数値1を入力してください->");
+        var a = Console.ReadLine();
+        Console.Write("数値2を入力してください->");
+        var b = Console.ReadLine();
+        Console.Write("数値3を入力してください->");
+        var c = Console.ReadLine();
+
+        insert[0] = int.Parse(a);
+        insert[1] = int.Parse(b);
+        insert[2] = int.Parse(c);
+
+        Console.WriteLine("入力結果");
+        Console.WriteLine($"数値1:{insert[0]}");
+        Console.WriteLine($"数値2:{insert[1]}");
+        Console.WriteLine($"数値3:{insert[2]}");
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -5,18 +5,16 @@ static class Program
 {
     static void Main(string[] args)
     {
-        // 2教科の平均点が合格ライン（60点）を超えているか？
-        int mathScore = 80;
-        int englishScore = 50;
-        int passingGrade = 60;
-
-        // 【優先順位のポイント】
-        // 1. カッコ ( ) の中が最優先
-        // 2. 次に割り算 / が行われる
-        // 3. 最後に比較 > が行われる
-        bool isPassed = (mathScore + englishScore) / 2 > passingGrade;
-
-        Console.WriteLine($"数学{mathScore}点、英語{englishScore}点。");
-        Console.WriteLine($"平均点は{passingGrade}点を超えているか：{isPassed}");
+        int[,] arraysA = new int[3, 3];
+        int[,] arraysB = { { 1, 2 }, { 4, 5 }, { 7, 8 } };
+        Console.WriteLine($"arraysB[0, 0] = {arraysB[0, 0]}");
+        Console.WriteLine($"arraysB[0, 1] = {arraysB[0, 1]}");
+        Console.WriteLine($"arraysB[0, 2] = {arraysB[0, 2]}");
+        Console.WriteLine($"arraysB[1, 0] = {arraysB[1, 0]}");
+        Console.WriteLine($"arraysB[1, 1] = {arraysB[1, 1]}");
+        Console.WriteLine($"arraysB[1, 2] = {arraysB[1, 2]}");
+        Console.WriteLine($"arraysB[2, 0] = {arraysB[2, 0]}");
+        Console.WriteLine($"arraysB[2, 1] = {arraysB[2, 1]}");
+        Console.WriteLine($"arraysB[2, 2] = {arraysB[2, 2]}");
     }
 }

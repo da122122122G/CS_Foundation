e442bfcb2c73ec504b7c3d1ee5537736b825482c
da122122122G <daichi.712.houzuki@gmail.com>
2026-04-28 16:12:23 +0900

    Ex_03_03

---
+++ Ex_03_03/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex_03_03;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Item item = new();    ///アイテム・インスタンスの生成
+
+        Console.Write("商品番号を入力してください->");
+        item.itemNo = int.Parse(Console.ReadLine());
+
+        Console.Write("商品名を入力してください->");
+        item.itemName = Console.ReadLine();
+
+        Console.Write("単価を入力してください->");
+        item.price = int.Parse(Console.ReadLine());
+
+        Console.Write("新しい単価を入力してください->");
+        item.newPrice = int.Parse(Console.ReadLine());
+
+        item.ChangePrice(item.newPrice);
+
+        item.Print();
+    }
+}

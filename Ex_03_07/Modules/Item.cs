172a88a82a4258f0c0def0b40257f78fd8a96d64
da122122122G <daichi.712.houzuki@gmail.com>
2026-04-30 13:39:05 +0900

    Ex_03_07

---
+++ Ex_03_07/Modules/Item.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_03_07.Modules
+{
+    public class Item
+    {
+        public int ItemNo { get; set; }
+        ///商品番号を表すプロパティ
+        public string ItemName { get; set; }
+        ///商品名を表すプロパティ
+        public int Price { get; set; }
+        ///単価を表すプロパティ
+        /// 
+        public void Print()
+        {
+
+        }
+    }
+}

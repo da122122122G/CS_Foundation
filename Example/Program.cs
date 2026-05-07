bf12965b28fda538fe1853296ea2e62b5bb4c776
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-07 12:30:59 +0900

    Json

--- Example/Program.cs
+++ Example/Program.cs
@@ -2,6 +2,8 @@
 using System.Globalization;
 using Microsoft.VisualBasic;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 namespace Example;
@@ -11,51 +13,20 @@ static class Program
 {
     static void Main(string[] args)
     {
-        List<int> intList = new List<int>();
-        intList.Add(6);
-        intList.Add(3);
-        intList.Add(5);
-        intList.Add(7);
+        var item = new Item() { Id = 100, Name = "みかん", Price = 150 };
 
-        Console.WriteLine($"intList[2]:{intList[2]}");
-        intList[2] = 10;
-        Console.WriteLine($"intList[2]:{intList[2]}");
+        //インスタンスをJson(文字列化)
+        var jsonStr = JsonConvert.SerializeObject(item);
+        Console.WriteLine(jsonStr);
 
-        Console.Write("一覧表示:");
-        ShowList(intList);
+        //Jsonからインスタンス化
+        var item2 = JsonConvert.DeserializeObject<Item>(jsonStr);
+        Console.WriteLine(item2);
 
-        Console.WriteLine($"intList.Count:{intList.Count}");
-        Console.WriteLine($"intList.Contains(5):{intList.Contains(5)}");
-        Console.WriteLine($"intList.Contains(9):{intList.Contains(9)}");
-
-        intList.Insert(1, 2);
-        Console.WriteLine("intList.Insert(1,2):");
-        Console.Write("一覧表示:");
-        ShowList(intList);
-
-        intList.Remove(3);
-        Console.WriteLine("intList.Remove(3):");
-        Console.Write("一覧表示:");
-        ShowList(intList);
-
-        intList.Sort();
-        Console.WriteLine("intList.Sort():");
-        Console.Write("一覧表示:");
-        ShowList(intList);
-
-        intList.Clear();
-        Console.WriteLine("intList.Clear():");
-        Console.Write("一覧表示:");
-        ShowList(intList);
-
-        static void ShowList(List<int> intList)
-        {
-            foreach (int i in intList)
-            {
-                Console.Write(i);
-            }
-            Console.WriteLine();
-        }
+        JObject jsonObj = JObject.Parse(jsonStr);
+        Console.WriteLine($"Id:{jsonObj["Id"]}");
+        Console.WriteLine($"Name:{jsonObj["Name"]}");
+        Console.WriteLine($"Price:{jsonObj["Price"]}");
     }
 }
 
